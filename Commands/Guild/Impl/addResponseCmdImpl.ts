73c3599b71f2a358c011983527993e1d695a9241
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-04-25 00:14:27 +0300

    include profanity filter

--- Commands/Guild/Impl/addResponseCmdImpl.ts
+++ Commands/Guild/Impl/addResponseCmdImpl.ts
@@ -8,7 +8,8 @@ import {commandType} from "../../../Entities/Generic/commandType";
 import {guildLoggerType} from "../../../Entities/Generic/guildLoggerType";
 import {loadSwearWords} from "../../../Queries/Generic/loadSwearWords";
 import {addMemberResponse} from "../../../Queries/Generic/MemberResponses";
-
+const profanity = require('profanity-js');
+const Profanity = new profanity();
 
 @injectable()
 export class AddResponseCmdImpl extends AbstractCommand implements addResponseCmd {
@@ -21,7 +22,9 @@ export class AddResponseCmdImpl extends AbstractCommand implements addResponseCm
 
     public async execute(receivedMessage: Message, receivedCommand: commandType, addGuildLog: guildLoggerType) {
         const swears = await loadSwearWords();
-        const nsfw = !!swears.find((swear) => receivedMessage.content.includes(swear['swear_word']));
+        const nsfw = swears.some((swear) =>
+            receivedMessage.content.includes(swear['swear_word']))
+        || Profanity.isProfane(receivedMessage.cleanContent);
         return addMemberResponse(receivedMessage.guild.id, receivedMessage.member.id, receivedCommand.commandless1, nsfw)
     }
 
