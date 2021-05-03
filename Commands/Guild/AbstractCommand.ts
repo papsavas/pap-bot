5b10d5459434d56c677d71abd12d4865f1c17124
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-05-03 15:52:33 +0300

    reinstalled inversify

--- Commands/Guild/AbstractCommand.ts
+++ Commands/Guild/AbstractCommand.ts
@@ -1,11 +1,11 @@
-import {injectable} from "Inversify";
-import {GenericCommand} from "./GenericCommand";
+import { injectable } from "Inversify";
+import { GenericCommand } from "./GenericCommand";
 import "reflect-metadata";
 import * as Discord from 'discord.js';
-import {Message} from 'discord.js';
-import {bugsChannel} from '../../index';
-import {commandType} from "../../Entities/Generic/commandType";
-import {guildLoggerType} from "../../Entities/Generic/guildLoggerType";
+import { Message } from 'discord.js';
+import { bugsChannel } from '../../index';
+import { commandType } from "../../Entities/Generic/commandType";
+import { guildLoggerType } from "../../Entities/Generic/guildLoggerType";
 
 @injectable()
 export abstract class AbstractCommand implements GenericCommand {
@@ -33,7 +33,7 @@ export abstract class AbstractCommand implements GenericCommand {
                 icon_url: "https://icon-library.com/images/error-icon-transparent/error-icon-transparent-13.jpg"
             },
             thumbnail: {
-                proxy_url: guild.iconURL({format: "png", size: 512})
+                proxy_url: guild.iconURL({ format: "png", size: 512 })
             },
             title: primaryCommandLiteral,
             color: "DARK_RED",
