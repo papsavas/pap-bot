700761a447d64b66ba921c432843fa0d6edfc21e
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-05-03 17:49:11 +0300

    imagine

--- dist/Queries/Generic/loadSwearWords.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadSwearWords = void 0;
-const CoreRepo_1 = require("../../DB/CoreRepo");
-function loadSwearWords() {
-    return CoreRepo_1.fetchTable('swear_words', ['swear_word']);
-}
-exports.loadSwearWords = loadSwearWords;
