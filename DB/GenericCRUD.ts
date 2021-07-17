dc7f233184c7187d5836fb05913bb6b185d46930
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-07-17 20:10:37 +0300

    set up CRUD

--- DB/GenericCRUD.ts
+++ DB/GenericCRUD.ts
@@ -3,14 +3,14 @@ import knex, { Knex } from "knex";
 import { v4 } from "uuid";
 import TableBuilder = Knex.TableBuilder;
 
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 class AbstractRepository {
     knex: Knex<any, unknown[]>;
     constructor() {
         this.knex = knex({
             client: 'pg',
-            connection: process.env.NODE_ENV == 'development' ? {
+            connection: process.env.NODE_ENV === 'development' ? {
                 host: process.env.DB_HOST,
                 port: parseInt(process.env.DB_PORT),
                 user: process.env.DB_USER,
@@ -36,49 +36,35 @@ class AbstractRepository {
             .table(tableName);
     }
 
-    fetchAllOnCondition(tableName: string, objClause: {}, returningFields = ['*']): Promise<any[]> {
+    fetchAllOnCondition(tableName: string, objClause: {}, returningFields = ['*']): Promise<object[]> {
         return this.knex
             .select(...returningFields)
             .table(tableName)
             .where(objClause);
     }
 
-    fetchFirstOnCondition(tableName: string, columnName: string, value: any, returningFields = ['*']): Promise<object> {
+    fetchFirstOnCondition(tableName: string, clause: {}, returningFields = ['*']): Promise<object> {
         return this.knex
             .select(...returningFields)
             .table(tableName)
-            .where(columnName, value)
+            .where(clause)
             .first();
     }
 
-    readFirstRow(table: string, column: string, value: string): Promise<any> {
-        return this.knex(table)
-            .where(column, value)
-            .first();
-
+    updateRow(tableName: string, clause: {}, newRow: {}, returnings?: string[]): Promise<any> {
+        return this.knex(tableName)
+            .where(clause)
+            .update(newRow, returnings)
+            .first()
     }
 
-    updateRow(tableName: string, column: string, value: string, newRow: {}, returnings?: string[]): Promise<any> {
+    updateRows(tableName: string, clause: {}, newRow: {}, returnings?: string[]): Promise<any> {
         return this.knex(tableName)
-            .where(column, value)
+            .where(clause)
             .update(newRow, returnings)
     }
 
-    updateRowOnMultConditions(tableName: string, objClause: {}, newRow: {}, returnings?: string[]): Promise<any> {
-        return this.knex(tableName)
-            .where(objClause)
-            .update(newRow, returnings)
-    }
-
-    async addRow(tableName: string, row, returnings?: string[]): Promise<any> {
-        if (await this.knex.schema.hasColumn(tableName, "uuid")) {
-            Object.assign(row, row, { "uuid": v4() });
-        }
-        return this.knex(tableName).insert(row)
-            .returning(returnings);
-    }
-
-    async addRows(tableName: string, rows: any[], returning?: string, size?: number): Promise<any> {
+    async addRows(tableName: string, rows: any[], returning?: string, size?: number) {
         if (await this.knex.schema.hasColumn(tableName, "uuid"))
             for (let row of rows)
                 Object.assign(row, { "uuid": v4() });
@@ -91,14 +77,104 @@ class AbstractRepository {
             .where(objClause)
             .del();
     }
+
+    dropRow(table: string, objClause: {}): Promise<number> {
+        return this.knex(table)
+            .where(objClause)
+            .first()
+            .del();
+    }
 }
 
 const DB = new AbstractRepository();
 
-type multitudeType = "one" | "1" | "all";
+type multitudeType = "one" | "1" | 1 | "all";
 
-/*
-export function read(multitude: multitudeType, tableName: string, clause: {}, returnings: string | string[]) {
+type createType = "integer"
+    | "bigInteger"
+    | "text"
+    | "string"
+    | "float"
+    | "double"
+    | "decimal"
+    | "boolean"
+    | "date"
+    | "time"
+    | "datetime"
+    | "json"
+    | "jsonb"
+    | "uuid"
+    | "array"
+    | "object"
+    | "enum"
 
+export function createData(
+    name: string,
+    columns: {
+        name: string,
+        type: createType,
+        nullable: boolean
+    }[],
+    timestamps: boolean = false
+) {
+    return DB.createTable(name, (tableBuilder: TableBuilder) => {
+        columns.forEach(col => {
+            col.nullable ?
+                tableBuilder[col.type](col.name) :
+                tableBuilder[col.type](col.name).notNullable();
+        });
+        tableBuilder.uuid('uuid')
+            .primary()
+            .unique()
+            .notNullable();
+        if (timestamps)
+            tableBuilder.timestamps(true, true)
+    });
 }
-*/
+
+export function addData(tableName: string, rows: {}[], returning?: string) {
+    return DB.addRows(tableName, rows, returning);
+}
+
+export async function readData(multitude: multitudeType, tableName: string, clause: {}, returnings: string[]): Promise<object[]> {
+    if (multitude === "all")
+        return DB.fetchAllOnCondition(
+            tableName,
+            clause,
+            returnings
+        );
+    else if (multitude === "one" || multitude === "1" || multitude === 1)
+        return [
+            await DB.fetchFirstOnCondition(
+                tableName,
+                clause,
+                Array.isArray(returnings) ? returnings : [returnings])
+        ];
+}
+
+export function updateData(multitude: multitudeType, tableName: string, clause: {}, newRow: {}, returnings?: string[]) {
+    if (multitude === "all")
+        return DB.updateRows(
+            tableName,
+            clause,
+            newRow,
+            returnings
+        );
+    else
+        return DB.updateRow(
+            tableName,
+            clause,
+            newRow,
+            returnings
+        );
+}
+
+export function deleteData(
+    multitude: multitudeType,
+    tableName: string,
+    clause: {}) {
+    if (multitude === "all")
+        return DB.dropRows(tableName, clause);
+    else
+        return DB.dropRow(tableName, clause);
+}
