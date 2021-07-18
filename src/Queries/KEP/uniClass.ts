9e77ec423cae06c0f1146e4a522402d439f5ebd9
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-07-18 16:45:47 +0300

    implement `teacher`, `uniClass` queries

---
+++ src/Queries/KEP/uniClass.ts
@@ -0,0 +1,21 @@
+import { deleteBatch, findOne, saveBatch } from "../../../DB/GenericCRUD";
+import { Teacher } from "../../Entities/KEP/Teacher";
+import { uniClass } from "../../Entities/KEP/uniClass";
+
+export function addUniClass(cl: uniClass) {
+    return saveBatch('class', [cl]);
+}
+
+export async function linkTeacherToUniClass(teacherUsername: string, classCode: string) {
+    const teacher = await findOne('teacher', { "username": teacherUsername }) as Teacher;
+    const uniClass = await findOne('class', { "code": classCode }) as uniClass;
+    return saveBatch('teacher_class', [{ "class_id": uniClass.uuid, "teacher_id": teacher.uuid }]);
+}
+
+export function deleteUniClass(code: uniClass['code']) {
+    return findOne('class', { "code": code }, ['uuid']).then(async (uniClass) => {
+        //!order matters, otherwise "teacher_class" will be violating foreign key constraint
+        await deleteBatch('teacher_class', { "class_id": uniClass['uuid'] });
+        await deleteBatch('class', { "uuid": uniClass['uuid'] });
+    });
+}
