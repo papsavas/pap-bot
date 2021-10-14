103c84e865ef2c5f6b6444c86b668138599e8d86
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-10-14 16:14:24 +0300

    feat: eventsCmd

--- src/Commands/Guild/Impl/KEP_eventsCmdImpl.ts
+++ src/Commands/Guild/Impl/KEP_eventsCmdImpl.ts
@@ -1,43 +1,79 @@
 import { ApplicationCommandData, Collection, CommandInteraction, Message, Snowflake } from "discord.js";
 import { guildMap } from "../../..";
+import { guildId } from "../../../../values/KEP/IDs.json";
 import { commandLiteral } from "../../../Entities/Generic/command";
+import { KepGuild } from "../../../Handlers/Guilds/Impl/KepGuild";
 import { fetchCommandID } from "../../../Queries/Generic/Commands";
+import { fetchEvents } from "../../../tools/Google/Gcalendar";
 import { AbstractGuildCommand } from "../AbstractGuildCommand";
 import { KEP_eventsCmd } from "../Interf/KEP_eventsCmd";
+
+const refreshLiteral = "refresh";
 
 export class KEP_eventsCmdImpl extends AbstractGuildCommand implements KEP_eventsCmd {
 
     protected _id: Collection<Snowflake, Snowflake>;
     protected _keyword = `events`;
     protected _guide = `Διαχειρίζεται τα events στο google calendar`;
-    protected _usage = ``;
+    protected _usage = `${this.keyword} ${refreshLiteral}}`;
     private constructor() { super() }
+
     static async init(): Promise<KEP_eventsCmd> {
         const cmd = new KEP_eventsCmdImpl();
         cmd._id = await fetchCommandID(cmd.keyword);
         return cmd;
     }
+
     private readonly _aliases = this.addKeywordToAliases
         (
-            [], this.keyword
+            ["event"], this.keyword
         );
+
     getCommandData(guild_id: Snowflake): ApplicationCommandData {
         return {
             name: this.keyword,
             description: this.guide,
-            type: 'CHAT_INPUT'
+            type: 'CHAT_INPUT',
+            options: [
+                {
+                    name: refreshLiteral,
+                    description: `Ανανεώνει τα events`,
+                    type: "SUB_COMMAND"
+                }
+            ]
         }
     }
     async interactiveExecute(interaction: CommandInteraction): Promise<unknown> {
+        const subCommand = interaction.options.getSubcommand(true);
+        return handleRequest(subCommand);
 
     }
-    async execute(message: Message, { }: commandLiteral): Promise<unknown> {
-
+    async execute(message: Message, { arg1 }: commandLiteral): Promise<unknown> {
+        return handleRequest(arg1);
     }
+
     getAliases(): string[] {
         return this._aliases;
     }
+
     addGuildLog(guildID: Snowflake, log: string) {
         return guildMap.get(guildID).addGuildLog(log);
     }
+}
+
+function handleRequest(subcommand: string) {
+    switch (subcommand) {
+
+        case refreshLiteral: {
+            return reloadEvents();
+        }
+
+        default:
+            return Promise.reject("invalid subcommand")
+    }
+}
+
+async function reloadEvents() {
+    const kep = guildMap.get(guildId) as KepGuild;
+    kep.events = await fetchEvents();
 }
