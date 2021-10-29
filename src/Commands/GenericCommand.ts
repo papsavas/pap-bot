c68169d9c00fdbe52c4e09b6be274eb9c5197ad8
Papageorgiadis Savvas <papsavas56@gmail.com>
2021-10-29 22:06:34 +0300

    fix(CommandManager): specify interaction type

--- src/Commands/GenericCommand.ts
+++ src/Commands/GenericCommand.ts
@@ -1,4 +1,4 @@
-import { Collection, CommandInteraction, Message, Snowflake } from "discord.js";
+import { Collection, CommandInteraction, ContextMenuInteraction, Message, Snowflake } from "discord.js";
 import { commandLiteral, commandSpecifier } from "../Entities/Generic/command";
 export interface GenericCommand {
     id: Collection<Snowflake, Snowflake>;
@@ -7,7 +7,7 @@ export interface GenericCommand {
     usage: string;
     type: commandSpecifier;
 
-    interactiveExecute(commandInteraction: CommandInteraction): Promise<unknown>;
+    interactiveExecute(commandInteraction: CommandInteraction | ContextMenuInteraction): Promise<unknown>;
 
     execute(receivedMessage: Message, receivedCommand: commandLiteral): Promise<unknown>;
 
