ed9c040293dfa6b678b39acb31af15ac68128229
Papageorgiadis Savvas <papsavas56@gmail.com>
2022-01-09 21:36:22 +0200

    fix(KEP_mute): use date instead of amount

--- src/Commands/Guild/Impl/KEP_muteCmdImpl.ts
+++ src/Commands/Guild/Impl/KEP_muteCmdImpl.ts
@@ -72,7 +72,7 @@ export class KEP_muteCmdImpl extends AbstractGuildCommand implements KEP_muteCmd
         await member.roles.add(muteRole);
         const unmuteAt = moment().add(amount, "hours");
         await saveMutedMember(member.id, unmuteAt, provoker_id, roles, reason);
-        await member.timeout(unmuteAt.milliseconds(), reason);
+        await member.disableCommunicationUntil(unmuteAt.toDate(), reason);
         const logs = interaction.guild.channels.cache.get(kepChannels.logs) as TextChannel;
         const headerEmb = new MessageEmbed({
             author: {
