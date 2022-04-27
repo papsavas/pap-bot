c6795811c28411194da20e6e7f5773e23a6771b9
Papageorgiadis Savvas <50584606+papsavas@users.noreply.github.com>
2022-04-27 21:38:34 +0300

    events: remove constructor and params (#159)

--- src/Events/AbstractEvent.ts
+++ src/Events/AbstractEvent.ts
@@ -5,15 +5,7 @@ import { GlobalCommandHandler } from "../Handlers/Global/GlobalCommandHandler";
 import GenericEvent from "./GenericEvent";
 
 export default abstract class AbstractEvent implements GenericEvent {
-    protected guilds: Guilds = null;
-    protected globalHandler: GlobalCommandHandler = null;
-    protected dmHandler: DmHandler = null;
     #name: keyof ClientEvents = null;
-    constructor(guilds: Guilds, globalHandler: GlobalCommandHandler, dmHandler: DmHandler) {
-        this.guilds = guilds;
-        this.globalHandler = globalHandler;
-        this.dmHandler = dmHandler;
-    }
 
     get name() {
         return this.#name;
